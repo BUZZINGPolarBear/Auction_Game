1c3d2b87eda513329413200ec55d4fb98dcc9bcb
suhwan <soo7652@naver.com>
2020-05-10 21:32:26 +0900

    first start project_ init setting

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\suhwan\desktop\comne_prj\auction_game\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
