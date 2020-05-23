fe4e8d7e97281170830c321782bc5c7a9ce3f226
전준휘(Jeon Juni) <35029025+BUZZINGPolarBear@users.noreply.github.com>
2020-05-23 20:58:54 +0900

    이미지 폰트 관전자 수정

--- server.js
+++ server.js
@@ -14,12 +14,20 @@ var current_price = 0;
 var item_count = 1;
 var raise_count = 0;
 var raise_name_list = new Array();
+var finish = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i'];
 // localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/betting', function(req, res) {
+  res.sendFile(__dirname + '/betting.html');
+});
+
+
 function start_game(){
+  io.emit('bet_start', item_count, item[item_count]);
+  io.emit('raise_price', price[item_count]);
   io.emit('start', item_count + "번째 경매 물품입니다.");
   io.emit('start', "물건 : " + item[item_count]);
   io.emit('start', "시작가 : " + price[item_count]);
@@ -36,6 +44,7 @@ function next_game(){
 }
 function upper_cost(){
   current_price += counting_price;
+  io.emit('raise_price', current_price);
   io.emit('start', "현재가 : " + current_price);
   io.emit('start', "호가 : " + counting_price);
   io.emit('start', "경매에 응하시겠습니까? (yes/no)");
@@ -44,9 +53,26 @@ function upper2_cost(){
   io.emit('start', "참여자 모두 입찰하여 호가를 2배 늘립니다.");
   current_price += counting_price;
   counting_price += counting_price;
+  io.emit('raise_price', current_price);
   io.emit('start', "현재가 : " + current_price);
   io.emit('start', "호가 : " + counting_price);
   io.emit('start', "경매에 응하시겠습니까? (yes/no)");
+}
+
+function Is_finish(){
+  //세로줄
+  for(var i = 0; i<3; i++){
+    if(finish[0+i] == finish[3+i] && finish[3+i] == finish[6+i]) return true;
+  }
+  //가로줄
+  for(var i = 0; i<3; i++){
+    if(finish[0+(3 * i)] == finish[1+(3 * i)] && finish[1+(3 * i)] == finish[2+(3 * i)]) return true;
+  }
+  //대각선
+  if(finish[0] == finish[4] && finish[4] == finish[8]) return true;
+  if(finish[2] == finish[4] && finish[4] == finish[6]) return true;
+  //빙고 안만들어진 경우
+  return false;
 }
 // connection event handler
 // connection이 수립되면 event handler function의 인자로 socket인 들어온다
@@ -55,8 +81,7 @@ io.on('connection', function(socket) {
 
   // 접속한 클라이언트의 정보가 수신되면
   socket.on('login', function(data) {
-    data.name += user_count;
-    data.userid += user_count;
+    data.userid = user_count;
     user_count++;
 
     console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
@@ -74,23 +99,19 @@ io.on('connection', function(socket) {
       start_game();
     }
   });
-
-  // 클라이언트로부터의 메시지가 수신되면
-  socket.on('chat', function(data) {
-    console.log('Message from %s: %s', socket.name, data.msg);
-
+  socket.on('bet', function(data){
+    console.log('bet Message from %s: %s', socket.name, data);
     var msg = {
       from: {
         name: socket.name,
         userid: socket.userid,
         wallet: socket.wallet,
       },
-      msg: data.msg
+      msg: data
     };
-    //경매 시 chat
-    if(data.msg == "no" || data.msg == "yes"){
+    if(data == "no" || data == "yes"){
     //경매에 응한경우
-    if(data.msg != "no") {
+    if(data != "no") {
       raise_name_list.push(msg);
       raise_count++;
     }
@@ -100,10 +121,15 @@ io.on('connection', function(socket) {
       bet_count = 0;
       if(raise_count == 1){
         io.emit('start', raise_name_list[0].from.name + " 참여자가 낙찰받았습니다!");
+        io.emit('bingo', raise_name_list[0].from.userid, item_count);
         raise_name_list[0].from.wallet -= current_price;
-        raise_name_list[0].from.win_count++;
+        //raise_name_list[0].from.win_count++;
         io.emit('start', "현재 잔여 금액 : " + raise_name_list[0].from.wallet);
-        next_game();
+        finish[item_count-1] = raise_name_list[0].from.name;
+        if(Is_finish()) {
+          io.emit('start', raise_name_list[0].from.name + " 참여자가 승리하였습니다!");
+        }
+        else next_game();
       }
       else if(raise_count == 2){
         upper_cost();
@@ -112,7 +138,19 @@ io.on('connection', function(socket) {
       raise_count = 0;
     }
   }
-    else io.emit('chat', msg);
+});
+  // 클라이언트로부터의 메시지가 수신되면
+  socket.on('chat', function(data) {
+    console.log('chat Message from %s: %s', socket.name, data.msg);
+    var msg = {
+      from: {
+        name: socket.name,
+        userid: socket.userid,
+        wallet: socket.wallet,
+      },
+      msg: data.msg
+    };
+    io.emit('chat', msg);
   });
 
 // force client disconnect from server
